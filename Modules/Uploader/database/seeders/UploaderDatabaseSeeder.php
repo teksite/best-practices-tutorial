27f32d3b64b94de9588266f07d7516c69be3edcf
teksite <teksite.design@gmail.com>
2026-04-18 17:40:46 +0330

    start to uploader module

---
+++ Modules/Uploader/database/seeders/UploaderDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Uploader\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UploaderDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/Uploader/resources/views/index.blade.php
@@ -0,0 +1,5 @@
+<x-uploader::layouts.master>
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('uploader.name') !!}</p>
+</x-uploader::layouts.master>
